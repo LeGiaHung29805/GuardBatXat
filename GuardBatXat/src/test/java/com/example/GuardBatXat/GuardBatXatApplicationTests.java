e34c31b17f20e4555a104885e8f77abfb4c4e484
Le Gia Hung <141727025+LeGiaHung29805@users.noreply.github.com>
2026-03-25 09:01:49 +0700

    update

---
+++ GuardBatXat/src/test/java/com/example/GuardBatXat/GuardBatXatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GuardBatXat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuardBatXatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
