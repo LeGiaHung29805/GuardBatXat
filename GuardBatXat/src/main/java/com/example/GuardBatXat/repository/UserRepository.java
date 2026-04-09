5089780e44f0dea918ae37396b2f3fb31c9d408d
lunaHieu <nguyenvanhieu040205@gmail.com>
2026-04-10 00:00:44 +0700

    fix : N+1 Query in user.java

--- GuardBatXat/src/main/java/com/example/GuardBatXat/repository/UserRepository.java
+++ GuardBatXat/src/main/java/com/example/GuardBatXat/repository/UserRepository.java
@@ -1,12 +1,20 @@
 package com.example.GuardBatXat.repository;
 
 import com.example.GuardBatXat.entity.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+    @Override
+@EntityGraph(attributePaths = {"role"})
+List<User> findAll();
+    @EntityGraph(attributePaths = {"role"})
+
     Optional<User> findByUsername(String username);
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
